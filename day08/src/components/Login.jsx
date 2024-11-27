ce9089139bf91324ac36f91b551ba72f9e33e2a7
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-27 21:23:21 +0530

    Code updated

--- day08/src/components/Login.jsx
+++ day08/src/components/Login.jsx
@@ -4,19 +4,36 @@ import { useNavigate } from "react-router-dom";
 export default function Login() {
     const [user, setUser] = useState({usename:"", password:"", error:""});
     const navigate = useNavigate();
-    
+    const getUserInputs=(e)=>{
+        const{name,value}=e.terget;
+        setUser((prev)=>({
+            ...prev,[name]:value
+        }))
+    }
+    const authentication=()=>{
+        if(user.usename==="admin" && user.password==="password123"){
+            setUser({usename:"", password:"", error:""});
+            navigate("/dashboard");
+        }else{
+            setUser({'error':"Invalid Username or Password"});
+        }
+    }
   return (
     <div>
         <table>
             <tr>
-                <td>
-                    <input
-                     type="text"
-                     value={userName} 
-                    />
-                </td>
+                <td>Username:</td>
+                <td><input type="text" name="usename" onChange={getUserInputs}/></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password" name="password" onChange={getUserInputs}/></td>
+            </tr>
+            <tr>
+                <td style={{color:'red'}} colspan={2}>Username:</td>
             </tr>
         </table>
+        <button onClick={authentication}>Login</button>
     </div>
-  )
+  );
 }
