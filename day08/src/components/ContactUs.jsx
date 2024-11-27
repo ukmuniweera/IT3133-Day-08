a1f6234dfe34bf863391e9a220f4d3a270dee176
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-27 18:51:49 +0530

    Components updated

--- day08/src/components/ContactUs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ContactUs() {
-  return (
-    <div>
-        <h1>ContactUs page</h1>
-    </div>
-  )
-}
--- day08/src/components/Login.jsx
+++ day08/src/components/Login.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Home() {
+export default function Login() {
   return (
     <div>
-        <h1>Home page</h1>
+        
     </div>
   )
 }
