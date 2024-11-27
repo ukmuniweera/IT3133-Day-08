81b05f38497df2031b5534892a6c9ea45015fed9
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-27 17:27:33 +0530

    Created components

--- day08/src/App.js
+++ day08/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/aboutus" element={<AboutUs/>}/>
+          <Route path="/contactus" element={<ContactUs/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
