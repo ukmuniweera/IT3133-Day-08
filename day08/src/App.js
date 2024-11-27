6e36958e41a19a4a8153498ff61cc9490190d7b7
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-11-27 17:46:19 +0530

    Test project setuped

--- day08/src/App.js
+++ day08/src/App.js
@@ -1,12 +1,19 @@
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Router>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/aboutus">AboutUs</Link></li>
+            <li><Link to="/contactus">ContactUs</Link></li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/home" element={<Home/>}/>
           <Route path="/aboutus" element={<AboutUs/>}/>
@@ -18,3 +25,4 @@ function App() {
 }
 
 export default App;
+              
